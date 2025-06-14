19ea1669a7e7a89b69414b4676e06cdf0334e4b9
spi <spi3@pm.me>
2025-06-14 19:28:43 -0400

    Added .profile config to macos setup

--- macos_setup.sh
+++ macos_setup.sh
@@ -5,3 +5,20 @@ echo "Running macOS setup..."
 
 echo "Installing Homebrew packages..."
 brew bundle
+
+# If ~/.profile directory does not exist then create it
+if [ ! -d "$HOME/.profile" ]; then
+  mkdir -p "$HOME/.profile"
+fi
+
+# Symlink the files within the macos/profile directory to ~/.profile
+for dotfile in macos/profile/*; do
+    FILE_NAME=`basename $dotfile`
+    FILE_HOME_NAME=$HOME/.profile/$FILE_NAME
+
+    rm -f $FILE_HOME_NAME
+
+    echo Linking $FILE_NAME to $FILE_HOME_NAME
+    ln -s $PWD/$dotfile $FILE_HOME_NAME
+    
+done
